b4b09ef54fe98800a3651aba1e42fee079f1552d
David Dollar <ddollar@gmail.com>
2009-02-18 01:00:28 -0500

    rewrote for rails 2.0

--- lib/eve-igb.rb
+++ lib/eve-igb.rb
@@ -1,16 +1,14 @@
-ActionController::MimeResponds::Responder.send :alias_method, :old_initialize, :initialize
+Mime::Type.register "text/x-eve-igb", :eve
+
 ActionController::MimeResponds::Responder.class_eval do
-  def initialize(block_binding)
-    old_initialize(block_binding)
-    user_agent = eval "request.env['HTTP_USER_AGENT']", block_binding
-    if user_agent.match(/^EVE-minibrowser/)
+  def initialize_with_eve_minibrowser(controller)
+    initialize_without_eve_minibrowser(controller)
+    if controller.request.user_agent.match(/^EVE-minibrowser/)
       @mime_type_priority = [ Mime::Type.lookup('text/x-eve-igb') ]
     end
   end
+  alias_method_chain :initialize, :eve_minibrowser
 end
-
-Mime::Type.register "text/x-eve-igb", :eve
-ActionController::MimeResponds::Responder::DEFAULT_BLOCKS[:eve] = %(Proc.new { render :action => "\#{action_name}.reve", :content_type => Mime::HTML })
 
 ActionController::Base.class_eval do
   def eve_trust_required
@@ -21,6 +19,3 @@ ActionController::Base.class_eval do
     end
   end
 end
-
-
-
